1477dcbed2576be2952abf8c995381ee764a71c9
SebaAlshedokhi <sebashedokhi@gmail.com>
2025-12-31 18:18:50 +0300

    Update user.py

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,4 +1,23 @@
 from app.models.BaseEntity import BaseModel
 
 
-class User(
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin = False):
+        super().__init__()
+
+        if not first_name or len(first_name) > 50:
+            raise ValueError("Invalid first_name")
+        if not last_name or len(last_name) > 50:
+            raise ValueError("Invalid last_name")
+        if not email:
+            raise ValueError("Email required")
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.places = []  # List to store related places
+
+    def add_place(self, place):
+        """Add a place to the user."""
+        self.places.append(place)
