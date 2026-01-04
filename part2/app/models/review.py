33be433802b9bcbf93cd6366ad6a2af004e2b959
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-04 16:49:08 +0300

    Update review.py

--- part2/app/models/review.py
+++ part2/app/models/review.py
@@ -5,13 +5,6 @@ class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
 
-        if not first_name or len(first_name) > 50:
-            raise ValueError("Invalid first_name")
-        if not last_name or len(last_name) > 50:
-            raise ValueError("Invalid last_name")
-        if not email:
-            raise ValueError("Email required")
-
         self.text = text
         self.rating = rating
         self.place = place
