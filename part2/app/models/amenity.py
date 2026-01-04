b41342f360483dbc33ccc00b1594e4c0620fca4c
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-04 16:46:30 +0300

    Update amenity.py

--- part2/app/models/amenity.py
+++ part2/app/models/amenity.py
@@ -1,1 +1,16 @@
+from app.models.BaseEntity import BaseModel
 
+
+class User(BaseModel):
+    def __init__(self, name):
+        super().__init__()
+
+        if not name or len(name) > 50:
+            raise ValueError("Invalid name")
+
+        self.name = name
+        self.places = []  # List to store related places
+
+    def add_place(self, place):
+        """Add a place to the user."""
+        self.places.append(place)
