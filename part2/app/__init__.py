482f323bccee5bfee1fe94342fcae3b50c76f801
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-19 18:45:49 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -10,6 +10,16 @@ def create_app(config_class="config.DevelopmentConfig"): #update to receive a co
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+----------------------
+    from yourapplication.model import db
+    db.init_app(app)
+
+    from yourapplication.views.admin import admin
+    from yourapplication.views.frontend import frontend
+    app.register_blueprint(admin)
+    app.register_blueprint(frontend)
+-----------------------
+    
     api = Api(
         app,
         version='1.0',
