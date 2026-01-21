68da0d1153c9469e660cb919cebaf8c528c81810
M_2005 <101177180+oDoDyK@users.noreply.github.com>
2026-01-21 18:43:16 +0300

    Update __init__.py

--- part2/app/models/__init__.py
+++ part2/app/models/__init__.py
@@ -1,1 +1,7 @@
+from hbnb.app.models.base_model import BaseModel
+from hbnb.app.models.user import User
+from hbnb.app.models.place import Place
+from hbnb.app.models.review import Review
+from hbnb.app.models.amenity import Amenity
 
+__all__ = ["BaseModel", "User", "Place", "Review", "Amenity"]
