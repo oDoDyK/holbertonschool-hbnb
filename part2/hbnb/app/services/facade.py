074226f4b3a7472d51a4d2ff4991223fac91839c
SebaAlshedokhi <sebashedokhi@gmail.com>
2026-01-25 18:07:45 +0300

    Update facade.py
    use the SQLAlchemyRepository instead of the InMemoryRepository Part3 task0

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -7,10 +7,10 @@ from hbnb.app.models.review import Review
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)  # Switched to SQLAlchemyRepository
+        self.place_repo = SQLAlchemyRepository(Place)
+        self.review_repo = SQLAlchemyRepository(Review)
+        self.amenity_repo = SQLAlchemyRepository(Amenity)
 
     # ===== User Management Methods =====
 
@@ -18,8 +18,8 @@ class HBnBFacade:
         user = User(**user_data)
         self.user_repo.add(user)
         return user
-
-    def get_user(self, user_id):
+    
+    def get_user_by_id(self, user_id):
         return self.user_repo.get(user_id)
 
     def get_all_users(self):
